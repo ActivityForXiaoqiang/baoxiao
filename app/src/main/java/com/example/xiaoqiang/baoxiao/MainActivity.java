62a8725b8302b0ebd0a0bf88e825697b424590a7
小强不小 <xiaoqiang_lol@163.com>
2018-04-30 01:32:10 +0800

    个人界面，上啦下啦框架，图片选择器

--- app/src/main/java/com/example/xiaoqiang/baoxiao/MainActivity.java
+++ app/src/main/java/com/example/xiaoqiang/baoxiao/MainActivity.java
@@ -1,20 +1,52 @@
 package com.example.xiaoqiang.baoxiao;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.xiaoqiang.baoxiao.common.base.MyBaseActivity;
+import com.example.xiaoqiang.baoxiao.common.ui.info.MineActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     /**
-     *
      * 小区
-     * */
+     */
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View headLayout = navigationView.getHeaderView(0);
+        headLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MineActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/xiaoqiang/baoxiao/common/ui/info/MineActivity.java
@@ -0,0 +1,61 @@
+package com.example.xiaoqiang.baoxiao.common.ui.info;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+
+import com.bumptech.glide.Glide;
+import com.example.xiaoqiang.baoxiao.R;
+import com.example.xiaoqiang.baoxiao.common.base.MyBaseActivity;
+import com.yanzhenjie.album.Action;
+import com.yanzhenjie.album.Album;
+import com.yanzhenjie.album.AlbumFile;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MineActivity extends MyBaseActivity {
+    private CircleImageView head;
+
+    private ArrayList<AlbumFile> mAlbumFiles;
+
+    @Override
+    public Integer getViewId() {
+        return R.layout.activity_mine;
+    }
+
+    @Override
+    public void init() {
+        head = findViewById(R.id.mine_head);
+        head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlbum();
+            }
+        });
+    }
+
+    void openAlbum() {
+
+        Album.album(this)
+                .multipleChoice()
+                .selectCount(1)
+                .camera(true)
+                .columnCount(4)
+                .checkedList(mAlbumFiles)
+                .onResult(new Action<ArrayList<AlbumFile>>() {
+                    @Override
+                    public void onAction(int requestCode, @NonNull ArrayList<AlbumFile> result) {
+                        Glide.with(MineActivity.this).load(result.get(0).getPath()).centerCrop().into(head);
+                    }
+                })
+                .onCancel(new Action<String>() {
+                    @Override
+                    public void onAction(int requestCode, @NonNull String result) {
+
+                    }
+                })
+                .start();
+
+    }
+}
